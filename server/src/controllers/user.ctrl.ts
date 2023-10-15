84bc4ad4378cbdd7b2a3166127a5ece35d2d342e
Selva Ganesh M <selvadev2k@gmail.com>
2023-10-15 23:21:54 +0530

    delete user

--- server/src/controllers/user.ctrl.ts
+++ server/src/controllers/user.ctrl.ts
@@ -28,6 +28,7 @@ const getUser = (req: Request<{userId: string}>, res: Response, next: NextFuncti
     .findById(req.params.userId)
     .lean()
     .then((user)=>{
+        if(!user) return next(new CustomError(404, 'user not found.'))
         return res.status(200).json({
             statusText: "success",
             message: "user fetch success.",
@@ -53,8 +54,53 @@ const getAllUsers = (req: Request, res: Response, next: NextFunction) => {
     .catch((err)=>next(err))
 }
 
+const updateUser = (req: Request<{userId: string}, {}, Partial<IUser>, {}>, res: Response, next: NextFunction) => {
+    let userId = req.params.userId;
+    if(!isValidMongoId(userId)) return next(new CustomError(400, "invalid userId is provided."));
+    let userPayload = req.body;
+    if (!userPayload || Object.keys(userPayload).length==0) 
+        return next(new CustomError(400, "user details is mandatory to update the user."));
+    userModel
+    .findByIdAndUpdate(userId, {
+        $set: userPayload
+    }, {
+        new: true
+    })
+    .lean()
+    .then(user=>{
+        if(!user) return next(new CustomError(404, 'user not found'))
+        return res.status(200).json({
+            statusText: "success",
+            message: "user updated successfully.",
+            payload: user 
+        })
+    })
+    .catch(err=>next(err));
+}
+
+const deleteUser = function(req: Request<{userId: string}>, res: Response, next: NextFunction){
+    let userId = req.params.userId;
+    if(!userId) return next(new CustomError(400, 'invalid userId provided'));
+    userModel
+    .deleteOne({
+        _id: userId
+    })
+    .lean()
+    .then(data=>{
+        if (data.deletedCount!=1) return next(new CustomError(404, "user not found."))
+        return res.status(200).json({
+            statusText: 'success',
+            message: 'user deleted successfully',
+            payload: null
+            })
+    })
+    .catch(err=>next(err))
+}
+
 export const userCtrl = {
     createUser,
     getUser,
     getAllUsers,
+    updateUser,
+    deleteUser
 }
