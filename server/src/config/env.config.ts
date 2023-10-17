d7e51fe906eff9f5b99faaf142ba05fa8d65de09
Selva Ganesh M <selvadev2k@gmail.com>
2023-10-17 15:15:19 +0530

    jwtSecret, envPresenceCheck

--- server/src/config/env.config.ts
+++ server/src/config/env.config.ts
@@ -1,10 +1,18 @@
 import dotenv from "dotenv"
 dotenv.config();
 
+
+let necessary_env_variables = ['PORT', 'MONGO_URI', 'JWT_SECRERT', 'SALT_ROUNDS']
+
 let saltRounds = parseInt(process.env.SALT_ROUNDS || "10");
 
 export const envs = {
     dbUri: process.env.MONGO_URI,
     port: process.env.PORT,
-    saltRounds
+    saltRounds,
+    jwtSecret: process.env.JWT_SECRET,
+    envPresenceCheck: ()=>{
+        let missing_vars = necessary_env_variables.filter(variable=>!process.env[variable]);
+        if (missing_vars.length>0) throw Error(`missing env varaibles: ${missing_vars.toString()}`)
+    }
 }
